7b1821a3884b39129f0939ea6ae3804d27480d3f
nantokanare <git@nantokanare.nl>
2024-01-02 00:02:17 +0100

    fast commit

--- conf.nix
+++ conf.nix
@@ -46,7 +46,7 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   networking.hostName = "${hostname}"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
