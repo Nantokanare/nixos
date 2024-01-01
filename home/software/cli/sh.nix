cf60050a05663f6b86d23888b2cc70cd7cc7e293
nantokanare <git@nantokanare.nl>
2024-01-01 20:59:15 +0100

    organizehe

--- home/software/cli/sh.nix
+++ home/software/cli/sh.nix
@@ -5,16 +5,18 @@
 }: let
   # My shell aliases
   myAliases = {
+    cdd = "cd ..";
     ls = "eza --icons -l -T -L=2";
     cat = "bat";
     htop = "btm";
     fd = "fd -Lu";
+    gt = "git add * && git commit -m \"fast commit\"";
     w3m = "w3m -no-cookie -v";
     neofetch = "disfetch";
     fetch = "disfetch";
     gfetch = "onefetch";
-    nixos-rebuild = "systemd-run --no-ask-password --uid=0 --system --scope -p MemoryLimit=16000M -p CPUQuota=60% nixos-rebuild";
-    home-manager = "systemd-run --no-ask-password --uid=1000 --user --scope -p MemoryLimit=16000M -p CPUQuota=60% home-manager";
+    #    nixos-rebuild = "systemd-run --no-ask-password --uid=0 --system --scope -p MemoryLimit=16000M -p CPUQuota=60% nixos-rebuild";
+    #    home-manager = "systemd-run --no-ask-password --uid=1000 --user --scope -p MemoryLimit=16000M -p CPUQuota=60% home-manager";
   };
 in {
   programs.fish = {
