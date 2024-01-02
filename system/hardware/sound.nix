60c1dc1af29efe31d36227d132b77e065503e38a
nantokanare <git@nantokanare.nl>
2024-01-02 01:47:14 +0100

    fast commit

--- system/hardware/sound.nix
+++ system/hardware/sound.nix
@@ -10,10 +10,9 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
+    wireplumber.enable = true;
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    media-session.enable = true;
   };
   sound.mediaKeys.enable = true;
 }
