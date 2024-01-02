e187374b6abf6807b430a02d64f9c1116e8616b8
nantokanare <git@nantokanare.nl>
2024-01-02 06:04:23 +0100

    fast commit

--- home/software/browser/firefox.nix
+++ home/software/browser/firefox.nix
@@ -1,41 +1,40 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}: {
   programs.firefox = {
     enable = true;
     profiles.default = {
-      bookmarks = {};
-      extensions = with pkgs.inputs.firefox-addons; [
-        ublock-origin
-        browserpass
+      bookmarks = [
+        {
+          name = "wikipedia";
+          tags = ["wiki"];
+          keyword = "wiki";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+        {
+          name = "kernel.org";
+          url = "https://www.kernel.org";
+        }
+        {
+          name = "Nix sites";
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "homepage";
+              url = "https://nixos.org/";
+            }
+            {
+              name = "wiki";
+              tags = ["wiki" "nix"];
+              url = "https://nixos.wiki/";
+            }
+          ];
+        }
       ];
-      bookmarks = {};
-      settings = {
-        "browser.disableResetPrompt" = true;
-        "browser.download.panel.shown" = true;
-        "browser.download.useDownloadDir" = false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.shell.defaultBrowserCheckCount" = 1;
-        "browser.startup.homepage" = "https://start.duckduckgo.com";
-        "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_ray    mondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"see    n":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"]    ,"currentVersion":18,"newElementCount":4}'';
-        "dom.security.https_only_mode" = true;
-        "identity.fxaccounts.enabled" = false;
-        "privacy.trackingprotection.enabled" = true;
-        "signon.rememberSignons" = false;
-      };
     };
-  };
-
-  home = {
-    persistence = {
-      # Not persisting is safer
-      # "/persist/home/misterio".directories = [ ".mozilla/firefox" ];
-    };
-  };
-
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = ["firefox.desktop"];
-    "text/xml" = ["firefox.desktop"];
-    "x-scheme-handler/http" = ["firefox.desktop"];
-    "x-scheme-handler/https" = ["firefox.desktop"];
   };
 }
