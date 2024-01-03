d3fdf13a54def2a9817ae54122804e6d677d2bca
nantokanare <git@nantokanare.nl>
2024-01-03 23:49:45 +0100

    fast commit

--- system/stylix.nix
+++ system/stylix.nix
@@ -8,10 +8,7 @@
 }: {
   stylix = {
     polarity = "dark";
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    image = ./../themes/walls/1.png;
     fonts = {
       serif = {
         package = pkgs.dejavu_fonts;
