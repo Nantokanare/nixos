4c4e0d501b515c19119a5b6e8f5dd302566cee6c
nantokanare <git@nantokanare.nl>
2024-01-04 09:00:22 +0100

    fast commit

--- home/software/cli/nvim.nix
+++ home/software/cli/nvim.nix
@@ -70,27 +70,28 @@
       #        '';
       #      }
 
-      #   {
-      lualine-nvim
-      #     type = "lua";
-      #     config = ''
-      #       local function metals_status()
-      #         return vim.g["metals_status"] or ""
-      #       end
-      #       require('lualine').setup(
-      #         {
-      #           sections = {
-      #             lualine_a = {'mode' },
-      #             lualine_b = {'branch', 'diff' },
-      #             lualine_c = {'filename', metals_status },
-      #             lualine_x = {'encoding', 'filetype'},
-      #             lualine_y = {'progress'},
-      #             lualine_z = {'location'}
-      #           }
-      #         }
-      #       )
-      #     '';
-      #   } # Status Line
+      {
+        plugin = lualine-nvim;
+        type = "lua";
+        config = ''
+          local function metals_status()
+            return vim.g["metals_status"] or ""
+          end
+          require('lualine').setup(
+            {
+              options = { theme = 'auto' },
+              sections = {
+                lualine_a = {'mode' },
+                lualine_b = {'branch', 'diff' },
+                lualine_c = {'filename', metals_status },
+                lualine_x = {'encoding', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+              }
+            }
+          )
+        '';
+      } # Status Line
       {
         plugin = nvim-treesitter.withAllGrammars; # Syntax Highlighting
         type = "lua";
