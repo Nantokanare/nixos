8481b9949fda4c1211c046d8b1825f6ea4ec6ec8
nantokanare <git@nantokanare.nl>
2024-01-04 02:56:30 +0100

    fast commit

---
+++ home/software/wm/sway.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  programs = {
+    sway = {
+      enable = true;
+      extraPackages = with pkgs; [
+        base16-schemes
+        basez
+        dmenu
+        feh
+        i3-ratiosplit
+        i3-resurrect
+        i3lock
+        i3nator
+        nitrogen
+        picom
+        pulseaudioFull
+        rofi
+        swaybg
+        swayfx
+        swayr
+        swayrbar
+        waybar
+        waypaper
+        wpaperd
+      ];
+    };
+    xwayland = {
+      enable = true;
+    };
+  };
+}
