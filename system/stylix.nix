0a8c976715f86428ca59e2ec137fad6cadc36055
nantokanare <git@nantokanare.nl>
2024-01-04 11:31:29 +0100

    fast commit

--- system/stylix.nix
+++ system/stylix.nix
@@ -9,7 +9,7 @@
   stylix = {
     autoEnable = true;
     polarity = "dark";
-    image = ./../themes/walls/2.jpg;
+    image = ./../themes/walls/2.png;
     opacity = {
       terminal = 0.7;
       desktop = 0.8;
