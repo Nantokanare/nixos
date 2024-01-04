ea9658a170e44fdaf7440373b070b8c0ad09c9c3
nantokanare <git@nantokanare.nl>
2024-01-04 06:07:41 +0100

    fast commit

--- home/software/wayland/sway.nix
+++ home/software/wayland/sway.nix
@@ -27,6 +27,35 @@
     systemd.enable = true;
   };
 
+  programs = {
+    sway = {
+      enable = true;
+      extraPackages = with pkgs; [
+        base16-schemes
+        basez
+        dmenu
+        feh
+        i3-ratiosplit
+        i3-resurrect
+        i3lock
+        i3nator
+        nitrogen
+        picom
+        pulseaudioFull
+        rofi
+        swaybg
+        swayfx
+        swayr
+        swayrbar
+        waybar
+        waypaper
+        wpaperd
+      ];
+    };
+    xwayland = {
+      enable = true;
+    };
+  };
   home.packages = with pkgs; [
     alacritty
     kitty
@@ -453,34 +482,5 @@
     #   settings = {
     #     color = "#" + config.lib.stylix.colors.base00;
     #   };
-  };
-  programs = {
-    sway = {
-      enable = true;
-      extraPackages = with pkgs; [
-        base16-schemes
-        basez
-        dmenu
-        feh
-        i3-ratiosplit
-        i3-resurrect
-        i3lock
-        i3nator
-        nitrogen
-        picom
-        pulseaudioFull
-        rofi
-        swaybg
-        swayfx
-        swayr
-        swayrbar
-        waybar
-        waypaper
-        wpaperd
-      ];
-    };
-    xwayland = {
-      enable = true;
-    };
   };
 }
