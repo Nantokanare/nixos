5f53a10832b81d3fc67420cf58a7c17040c7be55
nantokanare <git@nantokanare.nl>
2024-01-06 12:33:26 +0100

    fast commit

--- home/software/browser/firefox.nix
+++ home/software/browser/firefox.nix
@@ -1,22 +1,23 @@
-{ config, pkgs, ... }
 {
+  config,
+  pkgs,
+  ...
+}: {
+  home.file.".mozilla/firefox/default/user.js".source = builtins.fetchTarball {
+    url = git@github.com:arkenfox/user.js.git;
+  };
 
-home.file.".mozilla/firefox/your-profile-name/user.js".source = builtins.fetchTarball {
-  url = "https://github.com/arkenfox/user.js/archive/refs/tags/stable.tar.gz";
-  sha256 = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
-};
+  home.file.".mozilla/firefox/default/user.js".text = ''
+    // Arkenfox user.js preferences
+    user_pref("security.fileuri.strict_origin_policy", false);
+    // Add more preferences here as needed
+  '';
 
-home.file.".mozilla/firefox/your-profile-name/user.js".text = ''
-  // Arkenfox user.js preferences
-  user_pref("security.fileuri.strict_origin_policy", false);
-  // Add more preferences here as needed
-'';
-
-home.file.".mozilla/firefox/your-profile-name/policies.json".text = ''
-  {
-    "policies": {
-      // Add more policies here as needed
+  home.file.".mozilla/firefox/default/policies.json".text = ''
+    {
+      "policies": {
+        // Add more policies here as needed
+      }
     }
-  }
-'';
+  '';
 }
