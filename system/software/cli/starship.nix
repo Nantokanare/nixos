90a7b076ae53b7cb1a8b19bfd2a86c6e828d7c81
nantokanare <git@nantokanare.nl>
2024-01-15 13:58:14 +0100

    fast commit

--- system/software/cli/starship.nix
+++ system/software/cli/starship.nix
@@ -7,52 +7,53 @@
       character = {
         success_symbol = "[](bold green)";
         error_symbol = "[](bold red)";
-        battery = {
-          full_symbol = "🔋 ";
-          charging_symbol = "⚡️ ";
-          discharging_symbol = "💀 ";
-        };
-        erlang = {format = "via [e $version](bold red) ";};
-        git_branch = {symbol = "🌱 ";};
-        git_commit = {
-          commit_hash_length = 4;
-          tag_symbol = "🔖 ";
-        };
-        git_state = {
-          format = "[($state($progress_current of $progress_total))]($style) ";
-          cherry_pick = "[🍒 PICKING](bold red)";
-        };
-        git_status = {
-          conflicted = "🏳";
-          ahead = "🏎💨";
-          behind = "😰";
-          diverged = "😵";
-          untracked = "🤷‍";
-          stashed = "📦";
-          modified = "📝";
-          staged = "[++($count)](green)";
-          renamed = "👅";
-          deleted = "🗑";
-        };
-        hostname = {
-          ssh_only = false;
-          format = "on [work-box](bold red) ";
-          disabled = false;
-        };
-        nix_shell = {
-          disabled = false;
-          impure_msg = "[impure shell](bold red)";
-          pure_msg = "[pure shell](bold green)";
-          format = "via [☃️ $state( ($name))](bold blue) ";
-        };
-        terraform = {format = "[🏎💨 $version$workspace]($style) ";};
-        username = {
-          style_user = "cyan bold";
-          style_root = "red bold";
-          format = "[$user]($style) ";
-          disabled = false;
-          show_always = true;
-        };
+        #   battery = {
+        #     full_symbol = "🔋 ";
+        #     charging_symbol = "⚡️ ";
+        #     discharging_symbol = "💀 ";
+        #   };
+        #   erlang = {format = "via [e $version](bold red) ";};
+        #   git_branch = {symbol = "🌱 ";};
+        #   git_commit = {
+        #     commit_hash_length = 4;
+        #     tag_symbol = "🔖 ";
+        #   };
+        #   git_state = {
+        #     format = "[($state($progress_current of $progress_total))]($style) ";
+        #     cherry_pick = "[🍒 PICKING](bold red)";
+        #   };
+        #   git_status = {
+        #     conflicted = "🏳";
+        #     ahead = "🏎💨";
+        #     behind = "😰";
+        #     diverged = "😵";
+        #     untracked = "🤷‍";
+        #     stashed = "📦";
+        #     modified = "📝";
+        #     staged = "[++($count)](green)";
+        #     renamed = "👅";
+        #     deleted = "🗑";
+        #   };
+        #   hostname = {
+        #     ssh_only = false;
+        #     format = "on [work-box](bold red) ";
+        #     disabled = false;
+        #   };
+        #   nix_shell = {
+        #     disabled = false;
+        #     impure_msg = "[impure shell](bold red)";
+        #     pure_msg = "[pure shell](bold green)";
+        #     format = "via [☃️ $state( ($name))](bold blue) ";
+        #   };
+        #   terraform = {format = "[🏎💨 $version$workspace]($style) ";};
+        #   username = {
+        #     style_user = "cyan bold";
+        #     style_root = "red bold";
+        #     format = "[$user]($style) ";
+        #     disabled = false;
+        #     show_always = true;
+        #   };
+        # };
       };
     };
   };
