2957efb464fec85e4e866a0c56c12101f3a6836e
nantokanare <git@nantokanare.nl>
2024-01-17 22:03:08 +0100

    fast commit

--- home/home.nix
+++ home/home.nix
@@ -19,8 +19,11 @@
       # # Building this configuration will create a copy of 'dotfiles/screenrc' in
       # # the Nix store. Activating the configuration will then make '~/.screenrc' a
       # # symlink to the Nix store copy.
-      ".config/ironbar/config.json;".source = ./../../dotfiles/ironbar/config.json;
-
+      ".config/ironbar/config.json;".source = {
+        source = ./dotfiles/ironbar/config.json;
+        recursive = true;
+        executable = true;
+      };
       # # You can also set the file content immediately.
       # ".gradle/gradle.properties".text = ''
       #   org.gradle.console=verbose
