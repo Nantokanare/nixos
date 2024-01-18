7df323f6fc626630f19806b6be9eeae69206e769
nantokanare <git@nantokanare.nl>
2024-01-18 20:02:43 +0100

    fast commit

--- home/software/media/mpd.nix
+++ home/software/media/mpd.nix
@@ -7,7 +7,6 @@
     ympd.enable = true;
     mpd = {
       enable = true;
-      user = "${username}";
       dataDir = "/home/${username}/.config/mpd/dataDir";
       #    musicDirectory = "/home/${username}/media/music";
       #     extraConfig = ''
