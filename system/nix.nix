82ceeb9094b7b68e3da4ac39f971a06dd09171c6
nantokanare <git@nantokanare.nl>
2024-01-18 10:50:06 +0100

    fast commit

--- system/nix.nix
+++ system/nix.nix
@@ -1,16 +1,14 @@
 {pkgs, ...}: {
   nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      substituters = ["https://nix-gaming.cachix.org"];
-      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-    };
+    settings.experimental-features = ["nix-command" "flakes"];
+
     nixPath = [
       "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
       "nixos-config=$HOME/nix/flakesFromScratch/R9/system/conf.nix"
       "/nix/var/nix/profiles/per-user/root/channels"
     ];
 
+    # Ensure nix flakes are enabled
     package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
