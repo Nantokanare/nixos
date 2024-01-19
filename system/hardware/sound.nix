3f9dc444e2bf866b82660ec158bfeaedddecb72d
nantokanare <git@nantokanare.nl>
2024-01-19 11:08:19 +0100

    fast commit

--- system/hardware/sound.nix
+++ system/hardware/sound.nix
@@ -8,11 +8,12 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    jack = {
+      enable = true;
+      jackd.enable = true;
+      loopback.enable = true;
+    };
     wireplumber.enable = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
   };
   sound.mediaKeys.enable = true;
   environment.systemPackages = with pkgs; [
