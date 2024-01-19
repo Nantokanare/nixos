a7dcb8373a5fe4fce59e56cf7bc043524bd73b67
nantokanare <git@nantokanare.nl>
2024-01-19 12:21:06 +0100

    fast commit

--- system/software/shells/alias.nix
+++ system/software/shells/alias.nix
@@ -1,13 +1,15 @@
 {...}: {
   cdd = "cd ..";
-  ls = "eza --icons -l -T -L=2";
+  ls = "eza --icons";
+  ll = "eza --icons -l -T -L=1";
+  lll = "eza --icons -l -T -L=2";
   cat = "bat";
   fd = "fd -Lu";
   gt = "git add * && git commit -m \"fast commit\"";
   w3m = "w3m -no-cookie -v";
   fetch = "disfetch";
   gfetch = "onefetch";
-  ns = "nix-shell";
+  ns = "nix-shell -p";
   nrs = "sudo nixos-rebuild --flake . switch";
   hms = "home-manager --flake . switch";
   mpc = "mpc --host=''/run/user/1000/mpd/socket''";
